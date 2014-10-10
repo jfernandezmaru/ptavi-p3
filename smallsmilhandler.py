966ce08077f55bf30b6e9bc6dffc1470e59e3241
javier fernandez marugan <jmarugan@iota18.aulas.gsyc.es>
2014-10-10 15:50:02 +0200

    sin empezar get tags

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -5,7 +5,7 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-class SmallSMILHandler(ContentHandler)
+class SmallSMILHandler(ContentHandler):
 
     """
     Clase para manejar chistes malos
@@ -15,16 +15,26 @@ class SmallSMILHandler(ContentHandler)
         """
         Constructor. Inicializamos las variables
         """
+        
+#        self.inroot-layout = 0
+#        self.inregion = 0
+#        self.inimg = 0
+#        self.inaudio = 0
+#        self.intextstream = 0
+                        
         self.width = ""
         self.height = ""
-        self.background-color = ""
+        self.background_color = ""
         self.id = ""
         self.top = ""
-        self.left = 0
+        self.bottom = ""
+        self.left = ""
+        self.right = ""
         self.src = ""
         self.region = ""
         self.begin = ""
         self.dur = "0s"
+        self.end = "0s"
         
         self.fill = ""
 
@@ -35,13 +45,57 @@ class SmallSMILHandler(ContentHandler)
         """
         if name == 'root-layout':
 
+            self.width = attrs.get('width',"")
+            self.height = attrs.get('height',"")
+            self.background_color = attrs.get('background-color',"")
+            print self.width
+            print self.height
+            print self.background_color
+
         elif name == 'region':
+       
+            self.id = attrs.get('id',"")
+            self.top = attrs.get('top',"")
+            self.bottom = attrs.get('bottom', "")
+            self.left = attrs.get('left',"")
+            self.right = attrs.get('right',"")
+            print self.id
+            print self.top
+            print self.bottom
+            print self.left
+            print self.right
             
         elif name == 'img':
-       						
+
+#                                        hay varios src, dur, region se sobreescriben???  
+            self.src = attrs.get('src',"")
+            self.region = attrs.get('region',"")
+            self.begin = attrs.get('begin',"")
+            self.dur = attrs.get('dur',"")
+            self.end = attrs.get('end',"")
+            print self.src
+            print self.region
+            print self.begin
+            print self.dur
+            print self.end
+                               
         elif name == 'audio':
+
+            self.src = attrs.get('src',"")
+            self.begin = attrs.get('begin',"")
+            self.dur = attrs.get('dur',"")
+            print self.src
+            print self.begin
+            print self.dur
       
         elif name == 'textstream':
+
+            self.src = attrs.get('src',"")
+            self.region = attrs.get('region',"")
+            self.fill = attrs.get('fill', "")
+            print self.src
+            print self.region
+            print self.fill
 
     def endElement(self, name):
         """
@@ -49,15 +103,38 @@ class SmallSMILHandler(ContentHandler)
         """
         if name == 'root-layout':
 
+            self.width = ""
+            self.height = ""
+            self.background_color = ""
+
         elif name == 'region':
+      
+            self.id = ""
+            self.top = ""
+            self.bottom = ""
+            self.left = ""
+            self.right = ""
             
         elif name == 'img':
-       						
+
+#                                        hay varios src, dur, region se sobreescriben???  
+            self.src = ""
+            self.region = ""
+            self.begin = ""
+            self.dur = ""
+                               
         elif name == 'audio':
+
+            self.src = ""
+            self.begin = ""
+            self.dur = ""
       
         elif name == 'textstream':
-        
-#    def get_tags 
+
+            self.src = ""
+            self.region = ""
+            
+#    			def get_tags 
     
     def characters(self, char):
         """
@@ -69,24 +146,8 @@ if __name__ == "__main__":
     """
     Programa principal
     """
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil'))
 
